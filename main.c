05f2de78ffbd8a7b8d5f56c0fda0a37f44d81573
VictorVillegas2003 <78566953+VictorVillegas2003@users.noreply.github.com>
2021-02-11 15:25:38 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char CH1[]="Juan";
+    char CH2[]="Victor";
+    int Age1=17;
+    int Age2=17;
+    printf("There were two brothers called %s and %s\n", CH1, CH2);
+    printf("%s was %d and %s was %d\n", CH1, Age1, CH2, Age2);
+
+
+    return 0;
+}
